c629cf73c65cf575dc3de6e6be31aed547d30994
guoyufu-study <guoyufu_study@163.com>
2020-07-24 12:44:20 +0800

    4.2.3 有向图：可达性

---
+++ src/main/java/edu/princeton/cs/myalgs4/graphs/c42/DirectedDFS.java
@@ -0,0 +1,159 @@
+/******************************************************************************
+ *  编译:  javac DirectedDFS.java
+ *  执行:    java DirectedDFS digraph.txt s
+ *  依赖: Digraph.java Bag.java In.java StdOut.java
+ *  数据文件:   https://algs4.cs.princeton.edu/42digraph/tinyDG.txt
+ *                https://algs4.cs.princeton.edu/42digraph/mediumDG.txt
+ *                https://algs4.cs.princeton.edu/42digraph/largeDG.txt
+ *
+ *  使用深度优先搜索确定有向图中的单源或多源可达性。
+ *  运行时间 O(E + V) .
+ *
+ *  % java DirectedDFS tinyDG.txt 1
+ *  1
+ *
+ *  % java DirectedDFS tinyDG.txt 2
+ *  0 1 2 3 4 5
+ *
+ *  % java DirectedDFS tinyDG.txt 1 2 6
+ *  0 1 2 3 4 5 6 8 9 10 11 12 
+ *
+ ******************************************************************************/
+
+package edu.princeton.cs.myalgs4.graphs.c42;
+
+import edu.princeton.cs.algs4.fundamentals.Bag;
+import edu.princeton.cs.algs4.io.In;
+import edu.princeton.cs.algs4.io.StdOut;
+
+/**
+ *  The {@code DirectedDFS} class represents a data type for 
+ *  determining the vertices reachable from a given source vertex <em>s</em>
+ *  (or set of source vertices) in a digraph. For versions that find the paths,
+ *  see {@link DepthFirstDirectedPaths} and {@link BreadthFirstDirectedPaths}.
+ *  <p>
+ *  This implementation uses depth-first search.
+ *  The constructor takes time proportional to <em>V</em> + <em>E</em>
+ *  (in the worst case),
+ *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
+ *  Each instance method takes &Theta;(1) time.
+ *  It uses &Theta;(<em>V</em>) extra space (not including the digraph).
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class DirectedDFS {
+    private boolean[] marked;  // marked[v] = true iff v is reachable from source(s)
+    private int count;         // number of vertices reachable from source(s)
+
+    /**
+     * 计算图 {@code G} 中从给定源点 {@code s} 可达的顶点。
+     * @param G the digraph
+     * @param s the source vertex
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     */
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        validateVertex(s);
+        dfs(G, s);
+    }
+
+    /**
+     * 计算图 {@code G} 中从任一给定源点 {@code s} 可达的顶点。
+     * @param G the graph
+     * @param sources the source vertices
+     * @throws IllegalArgumentException if {@code sources} is {@code null}
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     *         for each vertex {@code s} in {@code sources}
+     */
+    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        validateVertices(sources);
+        for (int v : sources) {
+            if (!marked[v]) dfs(G, v);
+        }
+    }
+
+    private void dfs(Digraph G, int v) { 
+        count++;
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) dfs(G, w);
+        }
+    }
+
+    /**
+     * 顶点 {@code v} 是否可达？
+     * 
+     * @param  v the vertex
+     * @return {@code true} if there is a directed path, {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public boolean marked(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    /**
+     * 可达顶点的总数
+     * 
+     * @return the number of vertices reachable from the source vertex
+     *   (or source vertices)
+     */
+    public int count() {
+        return count;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertices(Iterable<Integer> vertices) {
+        if (vertices == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        for (Integer v : vertices) {
+            if (v == null) {
+                throw new IllegalArgumentException("vertex is null");
+            }
+            validateVertex(v);
+        }
+    }
+
+
+    /**
+     * 单元测试 {@code DirectedDFS} 数据类型。
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+
+        // 读图
+        Digraph G = new Digraph(new In(args[0]));
+
+        // 读源点
+        Bag<Integer> sources = new Bag<Integer>();
+        for (int i = 1; i < args.length; i++) {
+            int s = Integer.parseInt(args[i]);
+            sources.add(s);
+        }
+
+        // 多源点可达性
+        DirectedDFS dfs = new DirectedDFS(G, sources);
+
+        // 打印可达的顶点
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.marked(v)) StdOut.print(v + " ");
+        }
+        StdOut.println();
+    }
+
+}
